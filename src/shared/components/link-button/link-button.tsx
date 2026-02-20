3df342478eec5fb23133fe4012bd363159a8b650
Paweł Gr <pawelgrzybek2@wp.pl>
2026-02-20 16:47:41 +0100

    Add link button

---
+++ src/shared/components/link-button/link-button.tsx
@@ -0,0 +1,31 @@
+import { NavLink } from "react-router";
+import { Button } from "@/components/ui/button";
+
+interface LinkButtonProps extends React.ComponentProps<typeof Button> {
+  href: string;
+  external?: boolean;
+}
+
+const isExternalLink = (href: string) =>
+  /^(https?:\/\/|mailto:|tel:)/.test(href);
+
+export const LinkButton = ({
+  href,
+  external,
+  children,
+  ...props
+}: LinkButtonProps) => {
+  const isExternal = external ?? isExternalLink(href);
+
+  return (
+    <Button asChild {...props}>
+      {isExternal ? (
+        <a href={href} target="_blank" rel="noopener noreferrer">
+          {children}
+        </a>
+      ) : (
+        <NavLink to={href}>{children}</NavLink>
+      )}
+    </Button>
+  );
+};
