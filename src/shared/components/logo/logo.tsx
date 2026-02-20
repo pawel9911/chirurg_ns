9d1f1fe10699d40fe11677e0a9f43697b3533567
Paweł Gr <pawelgrzybek2@wp.pl>
2026-02-20 16:45:19 +0100

    Add logo

---
+++ src/shared/components/logo/logo.tsx
@@ -0,0 +1,52 @@
+import { NavLink } from "react-router";
+
+interface LogoProps {
+  size?: "regular";
+  color?: "light";
+  className?: string;
+}
+
+export const Logo = ({ size, color, className }: LogoProps) => {
+  const getSize = () => {
+    switch (size) {
+      case "regular": {
+        return {
+          width: 256,
+          height: 50,
+        };
+      }
+      default: {
+        return {
+          width: 200,
+          height: 40,
+        };
+      }
+    }
+  };
+
+  const getVariant = () => {
+    switch (color) {
+      case "light": {
+        return "logo_light.png";
+      }
+      default: {
+        return "logo.png";
+      }
+    }
+  };
+
+  const { width, height } = getSize();
+  const src = getVariant();
+
+  return (
+    <NavLink to="/">
+      <img
+        src={`/${src}`}
+        alt="logo"
+        width={width}
+        height={height}
+        className={className}
+      />
+    </NavLink>
+  );
+};
