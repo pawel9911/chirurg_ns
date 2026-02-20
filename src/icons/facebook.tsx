d147096a908047e8f39eb9102351e229636ad04c
Paweł Gr <pawelgrzybek2@wp.pl>
2026-02-20 16:56:27 +0100

    Add icons

---
+++ src/icons/facebook.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+interface FacebookProps extends React.SVGProps<SVGSVGElement> {
+  width?: number | string;
+  height?: number | string;
+}
+
+export const Facebook: React.FC<FacebookProps> = ({
+  width = 40,
+  height = 40,
+  ...props
+}) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={width}
+      height={height}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      {...props}
+    >
+      <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z" />
+    </svg>
+  );
+};
