1c6f459cc42b110e1097de747f5e3ad69cb5fd2f
Paweł Gr <pawelgrzybek2@wp.pl>
2026-02-23 09:12:04 +0100

    Adjust components from shadncn

--- src/components/ui/form.tsx
+++ src/components/ui/form.tsx
@@ -155,6 +155,7 @@ function FormMessage({ className, ...props }: ComponentProps<"p">) {
 }
 
 export {
+  // eslint-disable-next-line react-refresh/only-export-components
   useFormField,
   Form,
   FormItem,
