b4e3123a0766a709d68707e625555100573d3e08
Paweł Gr <pawelgrzybek2@wp.pl>
2026-02-23 14:15:11 +0100

    Fix type

--- src/shared/components/header/navigation-menu/navigation-submenu/navigation-submenu.tsx
+++ src/shared/components/header/navigation-menu/navigation-submenu/navigation-submenu.tsx
@@ -1,8 +1,8 @@
-import { PropsWithChildren } from "react";
+import { type PropsWithChildren } from "react";
 
 export const NavigationSubmenu = ({ children }: PropsWithChildren) => {
   return (
-    <ul className="grid gap-2 md:w-[500px] lg:w-[650px] lg:grid-cols-[1fr_1fr]">
+    <ul className="grid gap-2 md:w-125 lg:w-162.5 lg:grid-cols-[1fr_1fr]">
       {children}
     </ul>
   );
